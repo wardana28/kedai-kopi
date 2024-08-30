8d6b97cb618e33919e0f5dcae352c0dd3755a853
Angga Tri Wardana <triwardanaangga6@gmail.com>
2024-08-30 12:12:29 +0700

    membuat website kedai kopi

---
+++ script.js
@@ -0,0 +1,7 @@
+// class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
