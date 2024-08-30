fead553ddd99f9ac9d9ca74fb9268b7515dbc3c6
Angga Tri Wardana <triwardanaangga6@gmail.com>
2024-08-30 15:24:20 +0700

    menambahkan hero section

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// klik diluar sidebar untukmenghilangkan navbar
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
