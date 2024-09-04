a6fe61689caf2f10ca85cb7bde2812d37ca56411
Angga Tri Wardana <triwardanaangga6@gmail.com>
2024-09-04 18:19:41 +0700

    menambahkan fungsi search dan product

--- script.js
+++ script.js
@@ -2,14 +2,31 @@
 
 const navbarNav = document.querySelector(".navbar-nav");
 
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
 // klik diluar sidebar untukmenghilangkan navbar
-const hamburger = document.querySelector("#hamburger-menu");
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
+
+// toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  e.preventDefault();
+};
+
+// ketika klik diluar tombol search
