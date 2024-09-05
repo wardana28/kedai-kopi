4ccab39ac4f6b23dd6be56d1541849ca0c22e562
Angga Tri Wardana <triwardanaangga6@gmail.com>
2024-09-05 11:02:28 +0700

    menambahkan fungsi tombol shopping

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.querySelector("#hamburger-menu").onclick = (e) => {
 // klik diluar sidebar untukmenghilangkan navbar
 const hm = document.querySelector("#hamburger-menu");
 const sb = document.querySelector("#search-button");
+const sh = document.querySelector("#shop");
 
 document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
@@ -18,6 +19,10 @@ document.addEventListener("click", function (e) {
 
   if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
     searchForm.classList.remove("active");
+  }
+
+  if (!sh.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
   }
 });
 
@@ -30,3 +35,10 @@ document.querySelector("#search-button").onclick = (e) => {
 };
 
 // ketika klik diluar tombol search
+
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shop").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
